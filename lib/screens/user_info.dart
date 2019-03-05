10d86ab7fa7f066714a7312f2d6f150024f32ff7
Jacob Brook <brookjacob@gmail.com>
2019-03-05 15:48:55 -0800

    Added UserInfo Forms

--- lib/screens/user_info.dart
+++ lib/screens/user_info.dart
@@ -6,11 +6,47 @@ class UserInfo extends StatefulWidget {
 }
 
 class _UserInfoState extends State<UserInfo> {
+  final _clientKey = GlobalKey<FormState>();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('This is the user info page, it can be found in screens/user_info.dart'),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20, 20, 20, .1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              'Client Info',
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Flexible(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Name',
+                  labelText: 'Name',
+                ),
+              ),
+            ),
+            Flexible(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Phone',
+                  labelText: 'Phone',
+                ),
+              ),
+            ),
+            Flexible(
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+          ],
+        )
       )
     );
   }
