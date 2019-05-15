3a6d78a3887723b1519bc7ea3da5bddb3bcf290b
Utsal Shrestha <ushrestha20@my.whitworth.edu>
2019-05-15 15:09:39 -0700

    fixed inquiry page bug and email attachment bug

--- lib/screens/inquiry.dart
+++ lib/screens/inquiry.dart
@@ -19,14 +19,16 @@ class _InquiryPageState extends State<InquiryPage> {
               if (details.primaryVelocity.compareTo(0) == -1)
                 Navigator.of(context).pushNamed('/orderInfo');
             },
-            child: Container(
-              padding: EdgeInsets.fromLTRB(30,0,30,30),
+            child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[ Container(
+              padding: EdgeInsets.fromLTRB(20,0,20,20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
                 Image.asset('lib/icon_alpha.png'),
                 Text(
-                  'Welcome!\nThank you for choosing Happy Cake Company.\n\nThis app will allow you to conviently:\n\n-Get quotes for cakes/cupcakes\n-Reserve date for upcoming orders\n-Send pictures of cake concepts\n-Communicate with cake designers\n\nUpon submitting the inquiry form, you will receive an email confirmation. Through the email provided, a member of our team will follow-up and review your cake ideas and options for your order.\n\nThank you!',
+                  'Welcome!\nThank you for choosing Happy Cake Company.\n\nThis app will allow you to conveniently:\n\n► Get quotes for cakes/cupcakes\n► Reserve date for upcoming orders\n► Send pictures of cake concepts\n► Communicate with cake designers\n\nUpon submitting the inquiry form, you will receive an email confirmation. Through the email provided, a member of our team will follow-up and review your cake ideas and options for your order.\n\nThank you!',
                 ),
                 Container(
                     padding: EdgeInsets.only(top: 20),
@@ -42,6 +44,6 @@ class _InquiryPageState extends State<InquiryPage> {
                       },
                     ))
               ],
-            ))));
+            ))],)));
   }
 }
