9f366bf1141262be5555d24618d3b8b753f30731
Utsal Shrestha <ushrestha20@my.whitworth.edu>
2019-05-17 01:21:17 -0700

    alex's genius changes

--- lib/screens/confirmation.dart
+++ lib/screens/confirmation.dart
@@ -32,7 +32,8 @@ class _ConfirmationState extends State<Confirmation> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text('Thank you for choosing Happy Cake Company!\nWe will review your request and follow-up shortly.'),
+                          Text('Thank you for choosing Happy Cake Company!\n', style: TextStyle(fontStyle: FontStyle.italic, fontSize: 20)),
+                          Text( 'We will review your request and follow-up shortly.', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
                       ],),
                     ),
                     Container(padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
@@ -47,10 +48,10 @@ class _ConfirmationState extends State<Confirmation> {
                             )
                           ),
                           Text(
-                            '1312 N Mullan Rd.\nSpokane Valley, WA 99206\nTues-Sat 9-5pm\n509-924-8455',
+                            '1312 N Mullan Rd.\nSpokane Valley, WA 99206\nTues-Sat 9-5pm\n509-924-8455', style: TextStyle(fontSize:16, fontStyle: FontStyle.italic),
                           ),
                           Text(
-                            'Please call us with any additional info.',
+                            'Please call us with any additional info.',style: TextStyle(fontSize:16, fontWeight: FontWeight.bold)
                           ),
                       ],),
                     ),
